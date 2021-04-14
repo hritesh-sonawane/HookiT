184a5b32cdc394ccd2baef263d0efd12ef47fec5
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-14 22:07:04 +0530

    🔄  add Counter w/ useEffect

--- StateEffect/src/Container/AppFunction.js
+++ StateEffect/src/Container/AppFunction.js
@@ -3,6 +3,7 @@ import NewTask from "../Presentational/NewTask";
 import TasksList from "../Presentational/TasksList";
 import EditProfile from "../Presentational/EditProfile";
 import PageTitle from "../Presentational/PageTitle";
+import Counter from "../Presentational/Counter";
 
 export default function AppFunction() {
   const [newTask, setNewTask] = useState({}); // object in state
@@ -25,6 +26,8 @@ export default function AppFunction() {
   return (
     <main>
       <PageTitle />
+      <br />
+      <Counter />
       <br />
       <EditProfile />
       <h1>Tasks</h1>
