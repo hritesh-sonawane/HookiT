2fb46a37b3174a4c65e310109b8431efa517a99c
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-14 22:42:05 +0530

    🥝  update: add cleanup effects

--- StateEffect/src/Presentational/Counter.js
+++ StateEffect/src/Presentational/Counter.js
@@ -11,8 +11,19 @@ export default function Counter() {
     setCount((prevCount) => prevCount + 1);
   };
 
+  // Clean Up Effects
+  //   const [clickCount, setClickCount] = useState(0);
+  //   const increment = () => setClickCount((prev) => prev + 1);
+  //   useEffect(() => {
+  //     document.addEventListener("mousedown", increment);
+  //     return () => {
+  //       document.removeEventListener("mousedown", increment);
+  //     };
+  //   });
+
   return (
     <div>
+      {/* <h1>Document Clicks: {clickCount}</h1> */}
       <p>You clicked {count} times</p>
       <button onClick={handleClick}>Click me</button>
     </div>
