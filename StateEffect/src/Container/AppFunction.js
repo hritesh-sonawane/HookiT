d29ff5beb76bb5fdb5a0fec05e5306c005e6b770
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-15 12:06:50 +0530

    🎶  add Shop component w/ best practices

--- StateEffect/src/Container/AppFunction.js
+++ StateEffect/src/Container/AppFunction.js
@@ -1,48 +1,48 @@
-// import React, { useState } from "react";
-import React from "react";
-// import NewTask from "../Presentational/NewTask";
-// import TasksList from "../Presentational/TasksList";
-// import EditProfile from "../Presentational/EditProfile";
-// import PageTitle from "../Presentational/PageTitle";
-// import Counter from "../Presentational/Counter";
-// import Timer from "../Presentational/Timer";
-import Forecast from "../Presentational/Forecast";
+import React, { useState } from "react";
+import NewTask from "../Presentational/NewTask";
+import TasksList from "../Presentational/TasksList";
+import EditProfile from "../Presentational/EditProfile";
+import PageTitle from "../Presentational/PageTitle";
+import Counter from "../Presentational/Counter";
+import Timer from "../Presentational/Timer";
+import Shop from "../Presentational/Shop";
 
 export default function AppFunction() {
-  //   const [newTask, setNewTask] = useState({}); // object in state
-  //   const handleChange = ({ target }) => {
-  //     const { name, value } = target;
-  //     setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
-  //   };
+  const [newTask, setNewTask] = useState({}); // object in state
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+  };
 
-  //   const [allTasks, setAllTasks] = useState([]); // array in state
-  //   const handleSubmit = (event) => {
-  //     event.preventDefault();
-  //     if (!newTask.title) return;
-  //     setAllTasks((prev) => [newTask, ...prev]);
-  //     setNewTask({});
-  //   };
-  //   const handleDelete = (taskIdToRemove) => {
-  //     setAllTasks((prev) => prev.filter((task) => task.id !== taskIdToRemove));
-  //   };
+  const [allTasks, setAllTasks] = useState([]); // array in state
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask.title) return;
+    setAllTasks((prev) => [newTask, ...prev]);
+    setNewTask({});
+  };
+  const handleDelete = (taskIdToRemove) => {
+    setAllTasks((prev) => prev.filter((task) => task.id !== taskIdToRemove));
+  };
 
   return (
     <main>
-      {/* <Timer />
+      <Timer />
       <br />
       <PageTitle />
+      <br />
+      <Shop />
       <br />
       <Counter />
       <br />
       <EditProfile />
-      <h1>Tasks</h1>
+      {/* <h1>Tasks</h1>
       <NewTask
         newTask={newTask}
         handleChange={handleChange}
         handleSubmit={handleSubmit}
       />
       <TasksList allTasks={allTasks} handleDelete={handleDelete} /> */}
-      <Forecast />
     </main>
   );
 }
